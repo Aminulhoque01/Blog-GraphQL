78f1841d4369905bf543bf4d2d21c2a7a3f72ab8
Aminulhoque01 <aminuhoque53@gmail.com>
2025-03-15 10:41:45 +0600

    graphql schema

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,13 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './schema';
+import { resolvers } from './resolver';
 
 
-const typeDefs = `#graphql
-  
-  type Book {
-    title: String
-    author: String
-  }
-
- 
-  type Query {
-    books: [Book]
-  }
-`;
 
 
-const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
-    },
-];
 
 
-const resolvers = {
-    Query: {
-        books: () => books,
-    },
-};
 
 const main = async () => {
     const server = new ApolloServer({
