fd96fd0acceb23fd88fdd1d682da5235de26405f
Aminulhoque01 <aminuhoque53@gmail.com>
2025-03-15 11:48:55 +0600

    prisma graphql user get

--- src/resolver/index.ts
+++ src/resolver/index.ts
@@ -1,20 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
+
+interface userInfo{
+    name:string,
+    email:string,
+    password:string
+}
 
 export const resolvers = {
     Query: {
-        
+        users: async(parent:any, agrs:any, context:any)=>{
+            const user= await prisma.user.findMany();
+            return user
+            console.log(user)
+        }
     },
     Mutation:{
-        signup:async(parent:any, agrs:any,context:any)=>{
+        signup:async(parent:any, agrs:userInfo,context:any)=>{
            return await prisma.user.create({
             data: agrs
            })
             
-            
-            
+             
         }
     }
 };
